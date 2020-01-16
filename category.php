032889be938276c0d0545025229200e8cf258cf9
Shawn McGraw <shawn.mcgraw@protonmail.com>
2020-01-15 20:50:34 -0500

    fixed backgound image position

--- category.php
+++ category.php
@@ -8,10 +8,7 @@
         <?php if (has_post_thumbnail()) : ?>
           <?php $backgroundImg = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full'); ?>
         <?php endif ?>
-        <div class="container post-top mb-3 text-light" style="
-          background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('<?php echo $backgroundImg[0] ?>');
-          background-size: cover;
-        ">
+        <div class="container post-top mb-3 text-light" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('<?php echo $backgroundImg[0] ?>');">
           <h1 class="post-title stencil">
             <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
           </h1>
--- page.php
+++ page.php
@@ -8,11 +8,7 @@
       <?php if(has_post_thumbnail()) : ?>
           <?php $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
         <?php endif ?>
-      <div class="container post-top mb-3 text-light" 
-        style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('<?php echo $backgroundImg[0] ?>');
-        background-size: cover;
-        ">
-
+      <div class="container post-top mb-3 text-light" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('<?php echo $backgroundImg[0] ?>');">
         <h1 class="post-title stencil">
           <?php the_title(); ?>
         </h1>
